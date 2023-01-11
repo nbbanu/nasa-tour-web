aff6dfec3e65eaa15f61b29071f4b3667c9daf3c
nbbanu <banubkrli@gmail.com>
2023-01-11 14:06:27 +0300

    lock

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -11,3 +11,21 @@ hamburgerMenu.addEventListener("click", () => {
 closeMenuBtn.addEventListener("click", () => {
     sideMenu.classList.remove("active");
 })
+
+const dropdown = document.querySelector(".dropdown");
+const dropdownArrow = document.querySelector(".dd-arrow");
+const passengersDropdown = document.querySelector(".passengers-dropdown");
+const options = document.querySelectorAll(".option");
+const mainText = document.querySelector(".main-text");
+
+dropdownArrow.addEventListener("click",() => {
+    dropdown.classList.toggle("dropdown-active");
+});
+
+for (let option of options){
+    option.addEventListener("click", () => {
+        let optionText = option.querySelector(".option-text").innerText;
+        mainText.innerText = optionText;
+
+    });
+}
